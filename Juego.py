a4547a766f0f466e9332a58013dfbd68f3d1e1c6
Diego Pugliese <diego_pugliese91@hotmail.com>
2024-07-30 18:14:04 -0300

    Se agregan clases de jugador y enemigo

--- Juego.py
+++ Juego.py
@@ -3,7 +3,6 @@ import sys
 import random
 import time
 
-# Inicializamos PyGame
 pygame.init()
 
 # Colores
@@ -13,6 +12,12 @@ ROJO = (255, 0, 0)
 AZUL = (0, 0, 255)
 VERDE = (0, 255, 0)
 
+# Constantes
+TIEMPO_APARICION_GEMA_DIFIC_NORMAL = 10
+TIEMPO_APARICION_GEMA_DIFIC_DIFICIL = 20
+VELOCIDAD_MINIMA = 10
+VELOCIDAD_MAXIMA = 15
+
 # Dimensiones de la pantalla
 ANCHO = 800
 ALTO = 600
@@ -20,6 +25,41 @@ pantalla = pygame.display.set_mode((ANCHO, ALTO))
 
 # Reloj
 reloj = pygame.time.Clock()
+
+class Jugador:
+    def __init__(self, x, y, tamaño, color):
+        self.pos = [x, y]
+        self.tamaño = tamaño
+        self.color = color
+
+    def dibujar(self):
+        pygame.draw.rect(pantalla, self.color, (self.pos[0], self.pos[1], self.tamaño, self.tamaño))
+
+    def mover(self, teclas):
+        if teclas[pygame.K_LEFT] and self.pos[0] > 0:
+            self.pos[0] -= 5
+        if teclas[pygame.K_RIGHT] and self.pos[0] < ANCHO - self.tamaño:
+            self.pos[0] += 5
+        if teclas[pygame.K_UP] and self.pos[1] > 0:
+            self.pos[1] -= 5
+        if teclas[pygame.K_DOWN] and self.pos[1] < ALTO - self.tamaño:
+            self.pos[1] += 5
+
+class Enemigo:
+    def __init__(self, x, y, tamaño, color, velocidad):
+        self.pos = [x, y]
+        self.tamaño = tamaño
+        self.color = color
+        self.velocidad = velocidad
+
+    def mover(self):
+        self.pos[1] += self.velocidad
+        if self.pos[1] > ALTO:
+            self.pos[0] = random.randint(0, ANCHO - self.tamaño)
+            self.pos[1] = random.randint(-ALTO, 0)
+
+    def dibujar(self):
+        pygame.draw.rect(pantalla, self.color, (self.pos[0], self.pos[1], self.tamaño, self.tamaño))
 
 # Función para mostrar texto en la pantalla
 def mostrar_texto(pantalla, texto, tamaño, color, x, y):
@@ -33,10 +73,9 @@ def mostrar_texto(pantalla, texto, tamaño, color, x, y):
 def menu_principal():
     while True:
         pantalla.fill(NEGRO)
-        mostrar_texto(pantalla, "Juego", 74, BLANCO, ANCHO // 2, ALTO // 4)
+        mostrar_texto(pantalla, "Evadiendo", 74, BLANCO, ANCHO // 2, ALTO // 4)
         mostrar_texto(pantalla, "1. Elegir Modo", 36, BLANCO, ANCHO // 2, ALTO // 2)
         mostrar_texto(pantalla, "2. Salir", 36, BLANCO, ANCHO // 2, ALTO // 1.5)
-        mostrar_texto(pantalla, "Presiona Q para salir", 24, BLANCO, ANCHO // 2, ALTO // 1.2)
 
         for evento in pygame.event.get():
             if evento.type == pygame.QUIT:
@@ -86,20 +125,20 @@ def elegir_dificultad(modo):
                 sys.exit()
             if evento.type == pygame.KEYDOWN:
                 if evento.key == pygame.K_1:
-                    velocidad = 5
-                    tiempo_aparicion_gema = 10
+                    velocidad = VELOCIDAD_MINIMA
+                    tiempo_aparicion_gema = TIEMPO_APARICION_GEMA_DIFIC_NORMAL
                     modo()
                 if evento.key == pygame.K_2:
-                    velocidad = 7
-                    tiempo_aparicion_gema = 20
+                    velocidad = VELOCIDAD_MAXIMA
+                    tiempo_aparicion_gema = TIEMPO_APARICION_GEMA_DIFIC_DIFICIL
                     modo()
 
         pygame.display.update()
 
 # Función para el modo sobrevivir
 def juego_sobrevivir():
-    jugador_pos = [ANCHO // 2, ALTO - 50]
-    enemigos_pos = [[random.randint(0, ANCHO-50), random.randint(-ALTO, 0)] for _ in range(5)]
+    jugador = Jugador(ANCHO // 2, ALTO - 50, 50, AZUL) #centrar el jugador
+    enemigos = [Enemigo(random.randint(0, ANCHO - 50), random.randint(-ALTO, 0), 30, ROJO, velocidad) for _ in range(5)]
     start_time = time.time()
 
     while True:
@@ -109,29 +148,21 @@ def juego_sobrevivir():
                 sys.exit()
 
         teclas = pygame.key.get_pressed()
-        if teclas[pygame.K_LEFT] and jugador_pos[0] > 0:
-            jugador_pos[0] -= 5
-        if teclas[pygame.K_RIGHT] and jugador_pos[0] < ANCHO - 50:
-            jugador_pos[0] += 5
-        if teclas[pygame.K_UP] and jugador_pos[1] > 0:
-            jugador_pos[1] -= 5
-        if teclas[pygame.K_DOWN] and jugador_pos[1] < ALTO - 50:
-            jugador_pos[1] += 5
+        jugador.mover(teclas)
 
         pantalla.fill(NEGRO)
 
-        pygame.draw.rect(pantalla, AZUL, (jugador_pos[0], jugador_pos[1], 50, 50))
+        jugador.dibujar()
 
-        for pos in enemigos_pos:
-            pos[1] += velocidad
-            if pos[1] > ALTO:
-                pos[0] = random.randint(0, ANCHO-50)
-                pos[1] = random.randint(-ALTO, 0)
-            pygame.draw.rect(pantalla, ROJO, (pos[0], pos[1], 30, 30))
+        for enemigo in enemigos:
+            enemigo.mover()
+            enemigo.dibujar()
 
-        for pos in enemigos_pos:
-            if jugador_pos[0] < pos[0] + 30 and jugador_pos[0] + 50 > pos[0] and \
-               jugador_pos[1] < pos[1] + 30 and jugador_pos[1] + 50 > pos[1]:
+        for enemigo in enemigos:
+            if jugador.pos[0] < enemigo.pos[0] + enemigo.tamaño and \
+               jugador.pos[0] + jugador.tamaño > enemigo.pos[0] and \
+               jugador.pos[1] < enemigo.pos[1] + enemigo.tamaño and \
+               jugador.pos[1] + jugador.tamaño > enemigo.pos[1]:
                 mostrar_texto(pantalla, "¡Perdiste!", 74, BLANCO, ANCHO // 2, ALTO // 2)
                 pygame.display.update()
                 pygame.time.delay(2000)
@@ -140,7 +171,7 @@ def juego_sobrevivir():
         tiempo_transcurrido = int(time.time() - start_time)
         mostrar_texto(pantalla, f"Tiempo: {tiempo_transcurrido}", 36, BLANCO, 100, 50)
 
-        if tiempo_transcurrido >= 10:
+        if tiempo_transcurrido >= TIEMPO_APARICION_GEMA_DIFIC_NORMAL:
             mostrar_texto(pantalla, "¡Ganaste!", 74, BLANCO, ANCHO // 2, ALTO // 2)
             pygame.display.update()
             pygame.time.delay(2000)
@@ -151,8 +182,8 @@ def juego_sobrevivir():
 
 # Función para el modo conseguir gema
 def juego_conseguir_gema():
-    jugador_pos = [ANCHO // 2, ALTO - 50]
-    cuadrados_pos = [[random.randint(0, ANCHO-50), random.randint(-ALTO, 0)] for _ in range(5)]
+    jugador = Jugador(ANCHO // 2, ALTO - 50, 50, AZUL)
+    enemigos = [Enemigo(random.randint(0, ANCHO - 50), random.randint(-ALTO, 0), 30, ROJO, velocidad) for _ in range(5)]
     start_time = time.time()
     gema_visible = False
     gema_pos = None
@@ -164,46 +195,36 @@ def juego_conseguir_gema():
                 sys.exit()
 
         teclas = pygame.key.get_pressed()
-        if teclas[pygame.K_LEFT] and jugador_pos[0] > 0:
-            jugador_pos[0] -= 5
-        if teclas[pygame.K_RIGHT] and jugador_pos[0] < ANCHO - 50:
-            jugador_pos[0] += 5
-        if teclas[pygame.K_UP] and jugador_pos[1] > 0:
-            jugador_pos[1] -= 5
-        if teclas[pygame.K_DOWN] and jugador_pos[1] < ALTO - 50:
-            jugador_pos[1] += 5
+        jugador.mover(teclas)
 
         pantalla.fill(NEGRO)
 
-        pygame.draw.rect(pantalla, AZUL, (jugador_pos[0], jugador_pos[1], 50, 50))
+        jugador.dibujar()
 
-        for pos in cuadrados_pos:
-            pos[1] += velocidad
-            if pos[1] > ALTO:
-                pos[0] = random.randint(0, ANCHO-50)
-                pos[1] = random.randint(-ALTO, 0)
-            pygame.draw.rect(pantalla, ROJO, (pos[0], pos[1], 50, 50))
+        for enemigo in enemigos:
+            enemigo.mover()
+            enemigo.dibujar()
 
-        for pos in cuadrados_pos:
-            if jugador_pos[0] < pos[0] + 50 and jugador_pos[0] + 50 > pos[0] and \
-               jugador_pos[1] < pos[1] + 50 and jugador_pos[1] + 50 > pos[1]:
+        for enemigo in enemigos:
+            if jugador.pos[0] < enemigo.pos[0] + enemigo.tamaño and \
+               jugador.pos[0] + jugador.tamaño > enemigo.pos[0] and \
+               jugador.pos[1] < enemigo.pos[1] + enemigo.tamaño and \
+               jugador.pos[1] + jugador.tamaño > enemigo.pos[1]:
                 mostrar_texto(pantalla, "¡Perdiste!", 74, BLANCO, ANCHO // 2, ALTO // 2)
                 pygame.display.update()
                 pygame.time.delay(2000)
                 menu_principal()
 
-        tiempo_transcurrido = int(time.time() - start_time)
-        mostrar_texto(pantalla, f"Tiempo: {tiempo_transcurrido}", 36, BLANCO, 100, 50)
-
-        if tiempo_transcurrido >= tiempo_aparicion_gema and not gema_visible:
+        if not gema_visible and time.time() - start_time >= tiempo_aparicion_gema:
+            gema_pos = [random.randint(0, ANCHO - 50), random.randint(0, ALTO - 50)]
             gema_visible = True
-            gema_pos = [random.randint(0, ANCHO-50), random.randint(0, ALTO-50)]
 
         if gema_visible:
-            pygame.draw.rect(pantalla, VERDE, (gema_pos[0], gema_pos[1], 50, 50))
-
-            if jugador_pos[0] < gema_pos[0] + 50 and jugador_pos[0] + 50 > gema_pos[0] and \
-               jugador_pos[1] < gema_pos[1] + 50 and jugador_pos[1] + 50 > gema_pos[1]:
+            pygame.draw.rect(pantalla, VERDE, (gema_pos[0], gema_pos[1], 30, 30))
+            if jugador.pos[0] < gema_pos[0] + 30 and \
+               jugador.pos[0] + jugador.tamaño > gema_pos[0] and \
+               jugador.pos[1] < gema_pos[1] + 30 and \
+               jugador.pos[1] + jugador.tamaño > gema_pos[1]:
                 mostrar_texto(pantalla, "¡Ganaste!", 74, BLANCO, ANCHO // 2, ALTO // 2)
                 pygame.display.update()
                 pygame.time.delay(2000)
